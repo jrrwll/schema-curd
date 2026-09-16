b7001cea53586566740ac19e0d06d42bbfedf6e4
tuke <jrriwll@gmail.com>
2026-09-16 23:06:27 +0800

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+## 
+
+```shell
+cd be
+cargo test user::super_admin::test_insert_sql -- --nocapture
+```
--- be/tests/user/super_admin.rs
+++ be/tests/user/super_admin.rs
@@ -1,22 +1,16 @@
-use argon2::{
-    Argon2, PasswordHasher,
-    password_hash::{SaltString, rand_core::OsRng},
-};
-use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD};
-use rand_core::RngCore;
-use schema_curd::util::verify_password;
+use corers::rng_alphanumeric;
+use schema_curd::util::{hash_password, verify_password};
 
 #[tokio::test]
 async fn test_insert_sql() {
-    let mut password_bytes = [0_u8; 18];
-    OsRng.fill_bytes(&mut password_bytes);
-    let password = URL_SAFE_NO_PAD.encode(password_bytes);
-    let password_hash = Argon2::default()
-        .hash_password(password.as_bytes(), &SaltString::generate(&mut OsRng))
-        .unwrap()
-        .to_string();
+    let password = rng_alphanumeric(18);
+    let password_hash = hash_password(password.clone())
+        .await.unwrap();
 
-    println!("\nusername: super_admin");
+    println!("hash_password super_admin:\n{}\n", hash_password("super_admin".to_owned())
+        .await.unwrap());
+
+    println!("username: super_admin");
     println!("password: {password}\n");
     println!(
         "insert into sys_user (name, password, display_name, super_admin) values ('super_admin', '{}', 'Super Admin', 1);",
@@ -24,6 +18,5 @@ async fn test_insert_sql() {
     );
 
     let matched = verify_password(password.clone(), password_hash.clone()).await.unwrap();
-    println!("verify_password: {password} == {password_hash} is {matched}");
     assert!(matched);
 }
